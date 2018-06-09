26a3fe0f9465028d08bf370e9b2edae083473264
Filip Lindahl <gysztaf@gmail.com>
2018-06-09 21:34:22 +0200

    added prettier to project, ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,104 @@
-import * as React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Grid } from 'semantic-ui-react';
-import './App.css';
-import Team from './Team';
+import * as React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Grid } from "semantic-ui-react";
+import "./App.css";
+import Team from "./Team";
 
 class App extends React.Component<any, any> {
-    constructor(props: any) {
-	super(props);
-	this.switch = this.switch.bind(this);
-	this.incrementTeam1 = this.incrementTeam1.bind(this);
-	this.incrementTeam2 = this.incrementTeam2.bind(this);
-	this.fullscreenToggle = this.fullscreenToggle.bind(this);
-	this.state = { red: true, team1: 0, team2: 0, fullscreen: false };
-    }
+  constructor(props: any) {
+    super(props);
+    this.switch = this.switch.bind(this);
+    this.incrementTeam1 = this.incrementTeam1.bind(this);
+    this.incrementTeam2 = this.incrementTeam2.bind(this);
+    this.fullscreenToggle = this.fullscreenToggle.bind(this);
+    this.state = { red: true, team1: 0, team2: 0, fullscreen: false };
+  }
 
-    public switch() {
-	    this.setState({red: !this.state.red});
-    }
+  public switch() {
+    this.setState({ red: !this.state.red });
+  }
 
-    public incrementTeam1() {
-	this.setState((prevState: any, props: any) => ({
-	    team1: prevState.team1 + Math.floor(Math.random()*100)
-	}));
-    }
+  public incrementTeam1() {
+    this.setState((prevState: any, props: any) => ({
+      team1: prevState.team1 + Math.floor(Math.random() * 100)
+    }));
+  }
 
-    public incrementTeam2() {
-	this.setState((prevState: any, props: any) => ({
-	    team2: prevState.team2 + Math.floor(Math.random()*100)
-	}));
-    }
+  public incrementTeam2() {
+    this.setState((prevState: any, props: any) => ({
+      team2: prevState.team2 + Math.floor(Math.random() * 100)
+    }));
+  }
 
-    public fullscreenToggle() {
-	this.setState({fullscreen: !this.state.fullscreen});
-    }
+  public fullscreenToggle() {
+    this.setState({ fullscreen: !this.state.fullscreen });
+  }
 
-    public render() {
-        return (
+  public render() {
+    return this.state.fullscreen ? (
+      this.state.red ? (
+        <Team
+          className="container"
+          color="red"
+          buttonColor="blue"
+          teamName="Team 1"
+          switch={this.switch}
+          points={this.state.team1}
+          increment={this.incrementTeam1}
+          fullscreen={this.state.fullscreen}
+          fullscreenToggle={this.fullscreenToggle}
+        />
+      ) : (
+        <Team
+          className="container"
+          color="blue"
+          buttonColor="red"
+          teamName="Team 2"
+          switch={this.switch}
+          points={this.state.team2}
+          increment={this.incrementTeam2}
+          fullscreen={this.state.fullscreen}
+          fullscreenToggle={this.fullscreenToggle}
+        />
+      )
+    ) : (
+      <div className="container">
+        <Grid columns={2}>
+          <Grid.Column className="no-pad">
+            <Team
+              color="red"
+              buttonColor="blue"
+              teamName="Team 1"
+              switch={this.switch}
+              points={this.state.team1}
+              increment={this.incrementTeam1}
+            />
+          </Grid.Column>
 
-	    this.state.fullscreen ?
-	    this.state.red ?
-	    < Team className="container" color="red" buttonColor="blue" teamName="Team 1" switch = {this.switch} points = {this.state.team1} increment={this.incrementTeam1} fullscreen={this.state.fullscreen} fullscreenToggle={this.fullscreenToggle} /> :
-																						< Team className="container" color="blue" buttonColor="red" teamName="Team 2" switch = {this.switch} points = {this.state.team2} increment={this.incrementTeam2} fullscreen={this.state.fullscreen} fullscreenToggle={this.fullscreenToggle} />
-	    :
-	    < div className="container" >
-	    <Grid columns={2}>
+          <Grid.Column className="no-pad">
+            <Team
+              color="blue"
+              buttonColor="red"
+              teamName="Team 2"
+              switch={this.switch}
+              points={this.state.team2}
+              increment={this.incrementTeam2}
+            />
+          </Grid.Column>
+        </Grid>
 
-		<Grid.Column className="no-pad">
-		    < Team color="red" buttonColor="blue" teamName="Team 1" switch = {this.switch} points = {this.state.team1} increment={this.incrementTeam1} />
-		</Grid.Column>
-
-		<Grid.Column className="no-pad">
-		    < Team color="blue" buttonColor="red" teamName="Team 2" switch = {this.switch} points = {this.state.team2} increment={this.incrementTeam2} />
-		</Grid.Column>
-
-	    </Grid>
-
-	    <Button id="fullscreen" fluid={true} primary={true} size="mini" content="Fullscreen toggle" attached="bottom" onClick={this.fullscreenToggle} />
-	    </div>
-
-
-        );
-    }
+        <Button
+          id="fullscreen"
+          fluid={true}
+          primary={true}
+          size="mini"
+          content="Fullscreen toggle"
+          attached="bottom"
+          onClick={this.fullscreenToggle}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Team.tsx
+++ src/Team.tsx
@@ -1,54 +1,71 @@
-import * as React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { Button, Grid } from 'semantic-ui-react';
+import * as React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Grid } from "semantic-ui-react";
 
 class Team extends React.Component<any> {
-    constructor(props: any) {
-	super(props);
-    }
+  constructor(props: any) {
+    super(props);
+  }
 
-    public render() {
-        return (
-	    <div className={`${this.props.color} ${this.props.className||"shared"}`}>
-		<Grid rows={3} columns={3} textAlign="center">
-		    <Grid.Row verticalAlign="top">
-			<Grid.Column>
-			    <Button size="mini" floated="left" color={this.props.buttonColor} content="Switch" onClick={this.props.switch} />
-			</Grid.Column>
-			<Grid.Column/>
-			<Grid.Column>
-			    <Button size="mini" floated="right" color={this.props.buttonColor} content="Increment" onClick={this.props.increment} />
-			</Grid.Column>
-		    </Grid.Row>
-		    <Grid.Row verticalAlign="middle">
-			<Grid.Column />
-			<Grid.Column textAlign="center">
-			    <div className="content">
-				<h1>
-				    {this.props.teamName}
-				</h1>
-				<h2>
-				    Points:
-				    <p id="points">
-					{this.props.points}
-				    </p>
-				</h2>
-			    </div>
-			</Grid.Column>
-			<Grid.Column />
-		    </Grid.Row>
-		    <Grid.Row verticalAlign="bottom" />
+  public render() {
+    return (
+      <div
+        className={`${this.props.color} ${this.props.className || "shared"}`}
+      >
+        <Grid rows={3} columns={3} textAlign="center">
+          <Grid.Row verticalAlign="top">
+            <Grid.Column>
+              <Button
+                size="mini"
+                floated="left"
+                color={this.props.buttonColor}
+                content="Switch"
+                onClick={this.props.switch}
+              />
+            </Grid.Column>
+            <Grid.Column />
+            <Grid.Column>
+              <Button
+                size="mini"
+                floated="right"
+                color={this.props.buttonColor}
+                content="Increment"
+                onClick={this.props.increment}
+              />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column />
+            <Grid.Column textAlign="center">
+              <div className="content">
+                <h1>{this.props.teamName}</h1>
+                <h2>
+                  Points:
+                  <p id="points">{this.props.points}</p>
+                </h2>
+              </div>
+            </Grid.Column>
+            <Grid.Column />
+          </Grid.Row>
+          <Grid.Row verticalAlign="bottom" />
+        </Grid>
+        <Button
+          id="fullscreen"
+          fluid={true}
+          primary={true}
+          size="mini"
+          content="Fullscreen toggle"
+          attached="bottom"
+          onClick={this.props.fullscreenToggle}
+        />
+      </div>
+    );
+  }
 
-		</Grid>
-		<Button id="fullscreen" fluid={true} primary={true} size="mini" content="Fullscreen toggle" attached="bottom" onClick={this.props.fullscreenToggle} />
-	    </div >
-        );
-    }
-
-    public log() {
-        // tslint:disable-next-line:no-console
-        console.log("switch");
-    }
+  public log() {
+    // tslint:disable-next-line:no-console
+    console.log("switch");
+  }
 }
 
 export default Team;
