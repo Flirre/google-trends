a9f45f112e45dd01f2d21a0f17b821e8f37291fc
Filip Lindahl <filip.lindahl@fast2.se>
2020-01-03 23:39:48 +0100

    broke out internal component for cleaner jsx

--- client/src/Team.tsx
+++ client/src/Team.tsx
@@ -111,6 +111,43 @@ class Team extends React.Component<ITeamProps, ITeamState> {
     this.props.nextRound();
   }
 
+  public PointComponent = () => (
+    <div className="teamContainer">
+      <div className="middle center">
+        <h1 className="teamName">{this.state.name.toUpperCase()}</h1>
+
+        <h2>{this.props.points} POINTS</h2>
+      </div>
+    </div>
+  );
+
+  public SearchCompnent = () => (
+    <div className="chart-container">
+      <div className="input">
+        <Input
+          action={<Button icon="search" onClick={this.updateSearchTerm} />}
+          placeholder="Enter a search term"
+          onChange={this.onInputChange}
+          onKeyDown={this.keyPress}
+        />
+      </div>
+    </div>
+  );
+
+  public ChartComponent = () => (
+    <div className="chart-container">
+      <div className="chart">
+        <Chart
+          data={this.props.data}
+          color={this.props.color}
+          loaded={this.props.loaded}
+          team={this.props.team}
+          term={this.props.term}
+        />
+      </div>
+    </div>
+  );
+
   // TODO: Break apart Types into separate components
   public render() {
     return (
@@ -124,51 +161,9 @@ class Team extends React.Component<ITeamProps, ITeamState> {
             {!(this.props.type === Types.Search) ? (
               <Button className="next" onClick={this.signalReady} content=">" />
             ) : null}
-            {this.props.type === Types.Point ? (
-              <React.Fragment>
-                <div className="teamContainer">
-                  <div className="middle center">
-                    <h1 className="teamName">
-                      {this.state.name.toUpperCase()}
-                    </h1>
-
-                    <h2>{this.props.points} POINTS</h2>
-                  </div>
-                </div>
-              </React.Fragment>
-            ) : null}
-            {this.props.type === Types.Search ? (
-              <React.Fragment>
-                <div className="chart-container">
-                  <div className="input">
-                    <Input
-                      action={
-                        <Button icon="search" onClick={this.updateSearchTerm} />
-                      }
-                      placeholder="Enter a search term"
-                      onChange={this.onInputChange}
-                      onKeyDown={this.keyPress}
-                    />
-                  </div>
-                </div>
-                />
-              </React.Fragment>
-            ) : null}
-            {this.props.type === Types.Trend ? (
-              <React.Fragment>
-                <div className="chart-container">
-                  <div className="chart">
-                    <Chart
-                      data={this.props.data}
-                      color={this.props.color}
-                      loaded={this.props.loaded}
-                      team={this.props.team}
-                      term={this.props.term}
-                    />
-                  </div>
-                </div>
-              </React.Fragment>
-            ) : null}{' '}
+            {this.props.type === Types.Point ? <this.PointComponent /> : null}
+            {this.props.type === Types.Search ? <this.SearchCompnent /> : null}
+            {this.props.type === Types.Trend ? <this.ChartComponent /> : null}
           </>
         ) : (
           <>
